f2732fd38722aaf4f3a3e2e0186e6ca81286df09
m.bagautdinov <m.bagautdinov@fun-box.ru>
2020-02-04 22:21:13 +0400

    Add examples.

--- lib/subtract_time_ranges.rb
+++ lib/subtract_time_ranges.rb
@@ -1,5 +1,14 @@
+require 'helpers/parse_time_ranges'
+
 class SubtractTimeRanges
-  def call(input_string = '')
+  attr_accessor(:parse_time_ranges)
+
+  def initialize(parse_time_ranges: Helpers::ParseTimeRanges.new)
+    @parse_time_ranges = parse_time_ranges
+  end
+
+  def call(input_string)
+    ranges = parse_time_ranges.call(input_string)
     true
   end
 end
--- spec/lib/subtract_time_ranges_spec.rb
+++ spec/lib/subtract_time_ranges_spec.rb
@@ -1,7 +1,20 @@
 require 'subtract_time_ranges'
 
 RSpec.describe SubtractTimeRanges do
-  it 'returns true' do
-    expect(subject.call).to be_truthy
+  input_strings_with_results =
+    {
+      '(11:00-12:00) - (11:00-11:30)' => '(11:30-12:00)',
+      '(10:00-11:00) - (10:00-11:00)' => '()',
+      '(8:00-8:30) - (8:30-13:00)' => '(8:00-8:30)',
+      '(9:00-9:30, 10:00-10:30) - (9:15-10:15)' => '(9:00-9:15, 10:15-10:30)',
+      '(9:00-11:00, 13:00-15:00) - (9:00-9:15, 10:00-10:15, 12:30-16:00)' => '(9:15-10:00, 10:15-11:00)'
+    }
+
+  input_strings_with_results.each do |input_string, result|
+    context "when input_string is #{input_string}" do
+      it 'returns true' do
+        expect(subject.call(input_string)).to be_truthy
+      end
+    end
   end
 end
